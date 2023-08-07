3f1b4d20963a2ea22ca21fda2de5bfbd22a22b10
anuj <anuj@hosuing.com>
2023-08-07 17:16:45 +0530

    normal data

--- js/main.js
+++ js/main.js
@@ -145,11 +145,11 @@ options: {
     sensorChart.data.datasets[0].data.push(x);
     sensorChart.data.datasets[1].data.push(y);
     sensorChart.data.datasets[2].data.push(z);
-    if(sensorChart.data.labels.length >1000)
-        sensorChart.data.labels.shift()
-        sensorChart.data.datasets[0].data.shift();
-        sensorChart.data.datasets[1].data.shift();
-        sensorChart.data.datasets[2].data.shift();
+    // if(sensorChart.data.labels.length >1000)
+    //     sensorChart.data.labels.shift()
+    //     sensorChart.data.datasets[0].data.shift();
+    //     sensorChart.data.datasets[1].data.shift();
+    //     sensorChart.data.datasets[2].data.shift();
     sensorChart.update();
   }
   function updateChart1(timestamp, x, y, z) {
@@ -157,11 +157,11 @@ options: {
     sensorChart1.data.datasets[0].data.push(x);
     sensorChart1.data.datasets[1].data.push(y);
     sensorChart1.data.datasets[2].data.push(z);
-    if(sensorChart1.data.labels.length >1000)
-        sensorChart1.data.labels.shift()
-        sensorChart1.data.datasets[0].data.shift();
-        sensorChart1.data.datasets[1].data.shift();
-        sensorChart1.data.datasets[2].data.shift();
+    // if(sensorChart1.data.labels.length >1000)
+    //     sensorChart1.data.labels.shift()
+    //     sensorChart1.data.datasets[0].data.shift();
+    //     sensorChart1.data.datasets[1].data.shift();
+    //     sensorChart1.data.datasets[2].data.shift();
     sensorChart1.update();
   }
   function updateChart2(timestamp, x, y, z) {
@@ -169,11 +169,11 @@ options: {
     sensorChart2.data.datasets[0].data.push(x);
     sensorChart2.data.datasets[1].data.push(y);
     sensorChart2.data.datasets[2].data.push(z);
-    if(sensorChart2.data.labels.length >1000)
-        sensorChart2.data.labels.shift()
-        sensorChart2.data.datasets[0].data.shift();
-        sensorChart2.data.datasets[1].data.shift();
-        sensorChart2.data.datasets[2].data.shift();
+    // if(sensorChart2.data.labels.length >1000)
+    //     sensorChart2.data.labels.shift()
+    //     sensorChart2.data.datasets[0].data.shift();
+    //     sensorChart2.data.datasets[1].data.shift();
+    //     sensorChart2.data.datasets[2].data.shift();
     sensorChart2.update();
   }
   function updateChart3(timestamp, x, y, z) {
@@ -181,11 +181,11 @@ options: {
     sensorChart3.data.datasets[0].data.push(x);
     sensorChart3.data.datasets[1].data.push(y);
     sensorChart3.data.datasets[2].data.push(z);
-    if(sensorChart3.data.labels.length >1000)
-        sensorChart3.data.labels.shift()
-        sensorChart3.data.datasets[0].data.shift();
-        sensorChart3.data.datasets[1].data.shift();
-        sensorChart3.data.datasets[2].data.shift();
+    // if(sensorChart3.data.labels.length >1000)
+    //     sensorChart3.data.labels.shift()
+    //     sensorChart3.data.datasets[0].data.shift();
+    //     sensorChart3.data.datasets[1].data.shift();
+    //     sensorChart3.data.datasets[2].data.shift();
 
     sensorChart3.update();
   }
@@ -222,8 +222,8 @@ window.addEventListener('devicemotion', (event) => {
   prevTimestamp = timestamp;
   updateChart(timestamp, event.accelerationIncludingGravity.x, event.accelerationIncludingGravity.y, event.accelerationIncludingGravity.z);
   updateChart1(timestamp, event.acceleration.x, event.acceleration.y, event.acceleration.z);
-  updateChart2(timestamp, velocity.x, velocity.y, velocity.z);
-  updateChart3(timestamp, x, y, z);
+//   updateChart2(timestamp, velocity.x, velocity.y, velocity.z);
+//   updateChart3(timestamp, x, y, z);
   
   // Calculate and display estimated distance
   const distance = Math.sqrt(position.x ** 2 + position.y ** 2 + position.z ** 2);
